6a6c0151bc4b8013d7c4676bd76f37dde3b4597a
Mansi Rathod <mansir@apra.in>
2022-12-15 16:59:30 +0530

    Adding demo image

--- example/src/App.js
+++ example/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import logo from './Demo.png'
 
 import ReactPolygonDrawer from 'react-polygon-drawer'
-import 'react-polygon-drawer/dist/index.css'
 
 const App = () => {
-  const width = 300;
-  const height = 300;
+  const width = 400;
+  const height = 400;
   return <div style={{
     backgroundColor:'black',
     width,
     height
   }}>
+    <img src={logo}/>
     <ReactPolygonDrawer 
       width={width} 
       height={height} 
